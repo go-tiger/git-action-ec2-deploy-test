b405d96e5767759eb293f02f4c62fc6643f19b53
go-tiger <inter_tiger_@naver.com>
2023-03-24 02:48:20 +0900

    first

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+app.listen(3000, async () => {
+  console.log(`서버가 실행되었습니다. http://127.0.0.1:3000`);
+});
