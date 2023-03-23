2a9f887e13055f0171bdea0284ddb7d95ca01221
go-tiger <inter_tiger_@naver.com>
2023-03-24 04:14:09 +0900

    진짜 찐 마지막

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('hello test');
+  res.send('hello test 123123123');
 });
 
 app.listen(3000, async () => {
